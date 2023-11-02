970168263965a6f16d291d7c025d1ae5eb342cfc
divyashah008 <148768589+divyashah008@users.noreply.github.com>
2023-11-02 16:03:27 +0530

    Update BirthdayReminderSchController.java

--- src/main/java/com/reminder/controller/BirthdayReminderSchController.java
+++ src/main/java/com/reminder/controller/BirthdayReminderSchController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BirthdayReminderSchController {
 
 	
-	@Scheduled
+	//@Scheduled
 	public void birthdayRemScheduler() {
 		
 		
